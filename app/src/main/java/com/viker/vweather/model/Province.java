67e8241f2d8e18a968d39e26838999aaf5220322
Viker <454502552@qq.com>
2016-05-06 16:01:52 +0800

    新增数据库帮助类，以及各表对应的实体类。。

---
+++ app/src/main/java/com/viker/vweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.viker.vweather.model;
+
+/**
+ * Created by Viker on 2016/5/6.
+ * 创建每个数据表对应的实体类，方便于后续开发工作。新建Province类、City类、County类。
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
